48b01db9ea54919d99e98769ffe051549c734565
Anamol Khadka <anamolk@metropolia.fi>
2025-09-07 11:27:57 +0300

    Update module-1.py

--- module-1.py
+++ module-1.py
@@ -1,7 +1,8 @@
 # 1.
     # Write a program that greets you by your own name. If your name was Viivi Virta, the output of the program would be Hello, Viivi Virta!
 
-# name = 'Anamol'
-# name2 = 'Khadka'
-# print('Hello,' + ' ' + name + ' ' + name2 + '!')
+name = 'Anamol'
+name2 = 'Khadka'
+print('Hello,' + ' ' + name + ' ' + name2 + '!')
+
 
